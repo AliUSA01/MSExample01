e9f9f23141a8f684aff5d5e80d57e0e62e697e91
AliUSA01 <108878914+AliUSA01@users.noreply.github.com>
2023-12-27 02:24:26 +0300

    MS new Futures 2

--- src/Building.java
+++ src/Building.java
@@ -1,7 +1,9 @@
 
 public class Building {
     public static void main(String[] args) {
-        
+        System.out.println("Here we will share our new project of hospital plan");
+        System.out.println("The new project of Hospital location is located in: Grojecka 39");
+
     }
 
 
