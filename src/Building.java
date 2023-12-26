a11dc57d688909ca3054fff24c02f95ac5031e7a
AliUSA01 <108878914+AliUSA01@users.noreply.github.com>
2023-12-27 01:54:19 +0300

    MS new Futures 1

---
+++ src/Building.java
@@ -0,0 +1,8 @@
+
+public class Building {
+    public static void main(String[] args) {
+        System.out.println("Here we will share our new project of hospital plan");
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Ali Baba in the house ");
+        System.out.println("privet");
     }
 }
